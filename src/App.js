2476cd81c4950ad52ec5b824ea8b58487784414e
JonRider <jon@rider.net>
2022-03-13 19:37:43 -0400

    Alert Context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./users/Users";
@@ -7,41 +7,36 @@ import Search from "./users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/Aler  tState";
 
 import "./App.css";
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
-
-  // Send an alert that nothing was searched
-  const sendAlert = (msg, type) => {
-    setAlert({ msg, type });
-    setTimeout(() => setAlert(null), 3000);
-  };
-
   return (
     <GithubState>
-      <Router>
-        <div className="App">
-          <Navbar title="Github Finder" icon="fa fa-github" />
-          <div className="container">
-            <Alert alert={alert} />
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <Fragment>
-                    <Search sendAlert={sendAlert} />
-                    <Users />
-                  </Fragment>
-                }
-              />
-              <Route path="/about" element={<About />} />
-              <Route path={`/user/:login`} element={<User />} />
-            </Routes>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar title="Github Finder" icon="fa fa-github" />
+            <div className="container">
+              <Alert />
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <Fragment>
+                      <Search />
+                      <Users />
+                    </Fragment>
+                  }
+                />
+                <Route path="/about" element={<About />} />
+                <Route path={`/user/:login`} element={<User />} />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   );
 };
